103ef584796d3cc5a59573e707a42ba267d19a2f
Frédéric Romagné <frederic.romagne@eva.mpg.de>
2018-04-14 14:00:30 +0200

    implement Pooling, simplify the handling of sequences with 'N'

--- chunk_genome.py
+++ chunk_genome.py
@@ -2,6 +2,7 @@ from Bio import SeqIO
 import random
 import sys
 import numpy as np
+from multiprocessing import Pool
 import argparse
 
 parser = argparse.ArgumentParser(description='Split a genome into chuncks, add mutations')
@@ -9,6 +10,8 @@ parser.add_argument('--mut_rate', default=0.001, type=float,
                     help='Mutation rate')
 parser.add_argument('--num_seq', default=0, type=int,
                     help='Number of sequences to output')
+parser.add_argument('--nthreads', default=4, type=int,
+                    help='Number of threads to use')
 parser.add_argument('--outfile',  help='Fasta file where to extract sequence')
 parser.add_argument('--specie',  help='Specie Taxa')
 parser.add_argument('file_in', metavar="genome.fa")
@@ -39,6 +42,29 @@ def insert_mutations(seq):
         s[pos.item()] = random.sample(alphabet - set(s[pos]), 1)[0]
     return s
 
+def iterate_through_record(record):
+    i=0
+    tmp_chunks = []
+    while i < len(record):
+        # discard sequences with N only
+        if record[i] == 'N':
+            i+=1
+            continue
+        #default size is 35<>100
+        chunk_size = get_random_length()
+        #print ("Chunk",i,i+chunk_size, record[i:i+chunk_size].seq)
+        #get a subseq of the record
+        #chunk_record = record[i:i+chunk_size]                
+        #all_chunks.append([chunk_record, i])
+        
+        #reduce the size of the array for memory efficiency
+        tmp_chunks.append([record[i:i+chunk_size], i])
+        i += chunk_size
+    # control if our chunk contains enough reads
+    if args.num_seq and len(tmp_chunks) > int(args.num_seq/args.nthreads):
+        return random.sample(tmp_chunks, int(args.num_seq/args.nthreads))
+    else:
+        return tmp_chunks
 
 def main():
     all_chunks = []
@@ -47,31 +73,16 @@ def main():
     for record in SeqIO.parse(args.file_in, "fasta"):
         if check_kraken_id(record, args.specie): #start thread
             print("Found sequence for specie", args.specie, record.id, file=sys.stderr) 
-            i=0
-            tmp_chunks = []
-            while i < len(record):
-                # discard sequences with N only
-                for nuc in record[i:]:
-                    if nuc == 'N': i+=1
-                    else: break
-                #default size is 35<>100
-                chunk_size = get_random_length()
-                #print ("Chunk",i,i+chunk_size, record[i:i+chunk_size].seq)
-                #get a subseq of the record
-                #chunk_record = record[i:i+chunk_size]                
-                #all_chunks.append([chunk_record, i])
-                
-                #reduce the size of the array for memory efficiency
-                tmp_chunks.append([record[i:i+chunk_size], i])
-                i += chunk_size
-            if args.num_seq:
-                all_chunks += random.sample(tmp_chunks, args.num_seq)
-            else:
-                all_chunks += tmp_chunks
-            break
+            with Pool(args.nthreads) as p:
+                #TODO, check if specifying the chunk size can be directly in map...
+                thread_chunk_size = int(len(record) / args.nthreads)+1
+                tmp_chunks = p.map(iterate_through_record, [record[i*thread_chunk_size:(i+1)*thread_chunk_size] for i in range(args.nthreads)])
+            for thread_chunks in tmp_chunks:
+                all_chunks += thread_chunks
         # assume sequence are consecutive for each species 
-        elif len(all_chunks):#join threads
+        elif len(all_chunks):
             break
+    print("All sequences parsed, merging...", file=sys.stderr)
     if args.num_seq:
         all_chunks = random.sample(all_chunks, args.num_seq)
      
